e39b7559df72c771df2ab64366a6cb667d27891a
Victtorazzi <tiagovicttorazzi@gmail.com>
2026-07-09 21:49:31 -0300

    exercicios feitos de Listas

--- listas.py
+++ listas.py
@@ -1,2 +1,54 @@
 #_______________________________________________EXERCÍCIOS DE LISTAS E TUPLAS____________________________________________________
 
+# lista = ['maçã', 'banana', 'laranja', 'uva', 'abacaxi', 'morango', 'kiwi', 'melancia', 'pera', 'pêssego']
+
+# item = input('Digite o item que você quer verificar: ')
+
+# if item in lista:
+#     print(f'O item "{item}" está presente na despensa.')
+# else:
+#     print(f'O item "{item}" precisa ser comprado.')
+
+#===============================================================================
+
+# notas = input('Notas: ').split()
+
+# notas.sort()
+
+# print(f'Notas ordenadas: {notas}')
+
+#===============================================================================
+
+# voluntarios = []
+# nome = input('Digite o nome do voluntário: ')
+
+# while nome != 'sair':
+#     voluntarios.append(nome)
+#     nome = input('Digite o nome do voluntário: ')
+
+# print(f'Voluntários registrados: {voluntarios}')
+
+#===============================================================================
+
+# estoque1 = tuple(input('Produtos do estoque 1: ').split())
+# estoque2 = tuple(input('Produtos do estoque 2: ').split())
+
+# estoque_combinado = estoque1 + estoque2
+
+# print(f'Estoque combinado: \n{estoque_combinado}')
+
+#===============================================================================
+
+# lista = ['Ana', 'Pedro', 'Carlos']
+
+# print(f'Lista atual de convidados: {lista}')
+
+# novo_convidado = input('Digite o nome do novo convidado: ')
+# posicao = int(input('Digite a posição na qual deseja inserir o convidado: '))
+
+# lista.insert(posicao - 1, novo_convidado)
+
+# print(f'Lista atualizada de convidados: {lista}')
+
+#===============================================================================
+
