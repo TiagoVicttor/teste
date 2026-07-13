2a14f1814a143eabd98aa2bd5f7cc9b60d1efeea
Victtorazzi <tiagovicttorazzi@gmail.com>
2026-07-13 20:37:02 -0300

    lista de dicionários e conjuntos completa

--- dic.py
+++ dic.py
@@ -49,21 +49,103 @@
 
 #===============================================================================
 
-equipeA = {'planejar reunião', 'revisar documento', 'testar sistema'}
+# equipeA = {'planejar reunião', 'revisar documento', 'testar sistema'}
 
-equipeB = {'testar sistema', 'implementar funcionalidades', 'corrigir bugs'}
+# equipeB = {'testar sistema', 'implementar funcionalidades', 'corrigir bugs'}
 
-tarefas_finais = equipeA.union(equipeB)
+# tarefas_finais = equipeA.union(equipeB)
 
-print(f'Equipe A: {equipeA}')
+# print(f'Equipe A: {equipeA}')
 
-print(f'Equipe B: {equipeB}')
+# print(f'Equipe B: {equipeB}')
 
-print(f'Tarefas combinadas: {tarefas_finais}')
+# print(f'Tarefas combinadas: {tarefas_finais}')
 
-pergunta = input('Deseja remover alguma tarefa: ').lower()
+# pergunta = input('Deseja remover alguma tarefa: ').lower()
 
-if pergunta in tarefas_finais:
-    tarefas_finais.remove(pergunta)
+# if pergunta in tarefas_finais:
+#     tarefas_finais.remove(pergunta)
 
-print(f'Tarefas finais: {tarefas_finais}')
+# print(f'Tarefas finais: {tarefas_finais}')
+
+#======================================================================================
+
+
+# produto = {}
+
+# for i in range(3):
+#     nome = input('Digite o nome do produto: ')
+#     quantidade = int(input('Digite a quantidade: '))
+#     produto[nome] = quantidade
+
+# print(f'Dicionário de produtos: {produto}')
+
+#=======================================================
+
+# estoque = {'caderno universitário': 50, 'caneta azul': 120, 'borracha branca':30}
+
+# produto = input('Nome do produto a ser atualizado: ')
+
+
+# if produto in estoque:
+#     quantidade = int(input('Digite a nova quantidade: '))
+#     estoque[produto] = quantidade
+#     print(estoque)
+# else:
+#     print(f'O produto {produto} não está no estoque.')
+
+#=======================================================================================
+
+# participantes = {'Mariana':25, 'Carlos':32, 'Beatriz':28, 'Rafael':35}
+
+# print(f'Nome dos participantes: {(', '.join(participantes.keys()))}')
+
+# print(f'Idades dos participantes: {(', '.join(str(idade) for idade in participantes.values()))}')
+
+# print('Participantes e suas idades: ')
+
+# for nome, idade in participantes.items():
+#     print(f'-{nome}: {idade}')
+
+#=========================================================================================================
+
+# participantes = {'Workshop 1': {'Alice', 'Bruno', 'Carla', 'Diego'}, 'Workshop 2': {'Fernanda', 'Gustavo', 'Helena'}}
+
+# print(f'Participantes: {', '.join(str(nome) for nome in participantes.values())}')
+
+# remover = input('Nome do participante a ser removido: ')
+
+# for evento, nomes in participantes.items():
+#     nomes.discard(remover)
+
+# print('Lista de participantes atualizada: ')
+
+# for evento, nomes in participantes.items():
+#     print(f'-{evento}: {nomes}')
+
+#===========================================================================================================
+
+# vendas = {
+#     'Eletronicos': [
+#         {'produto': 'Smartphone', 'Quantidade': 5, 'Valor': 2000},
+#         {'produto': 'Table', 'Quantidade': 3, 'Valor': 1500}
+#     ],
+
+#     'Eletrodomésticos': [
+#         {'produto': 'Geladeira', 'Quantidade': 2, 'Valor': 3000},
+#         {'produto': 'Micro-ondas', 'Quantidade': 4, 'Valor': 800}
+#     ],
+
+#     'Livros': [
+#         {'produto': 'Livro A', 'Quantidade': 10, 'Valor': 50},
+#         {'produto': 'Livro B', 'Quantidade': 53, 'Valor': 100}
+#     ]
+# }
+
+# print('Total de vendas por categoria: ')
+
+# for categoria, itens in vendas.items():
+#     total = 0
+#     for item in itens:
+#         total += item['Quantidade'] * item['Valor']
+#     print(f'-{categoria}: R${total:.2f}')
