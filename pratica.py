dda1c32dfa879f9af13b7b10624afda09bc95d10
Victtorazzi <tiagovicttorazzi@gmail.com>
2026-07-25 19:18:51 -0300

    Mais alguns exercícios feitos

--- pratica.py
+++ pratica.py
@@ -9,20 +9,46 @@
 
 #=======================================================
 
-def verificar(cpf):
-    if not cpf.isdigit():
-        return 'Erro: O CPF de conter apenas dígitos'
-    elif len(cpf) > 11 or len(cpf) < 11:
-        return 'Erro: O CPF deve ter exatamente 11 dígitos'
-    else:
-        return 'CPF válido'
+# def verificar(cpf):
+#     if not cpf.isdigit():
+#         return 'Erro: O CPF de conter apenas dígitos'
+#     elif len(cpf) > 11 or len(cpf) < 11:
+#         return 'Erro: O CPF deve ter exatamente 11 dígitos'
+#     else:
+#         return 'CPF válido'
 
 
-cpf = input('Digite seu CPF: ')
-cpf_verificado = verificar(cpf)
+# cpf = input('Digite seu CPF: ')
+# cpf_verificado = verificar(cpf)
 
-print(cpf_verificado)
+# print(cpf_verificado)
 
+#===================================================================
 
+# def contador_vogais(texto):
+#     vogais = 'aeiou'
+#     quantidade = 0
 
+#     for letra in texto.lower():
+#         if letra in vogais:
+#             quantidade+=1
+#     return quantidade
 
+# texto = input('Digite o texto: ')
+
+# print(f'O texto contém {contador_vogais(texto)} vogais.')
+
+#=======================================================================
+
+texto = input('Digite um texto: ')
+
+palavras_longas = []
+
+for palavra in texto.split():
+    if len(palavra)>10:
+        palavras_longas.append(palavra)
+
+if palavras_longas:
+    print(f'Palavras longas encontradas: {", ".join(palavras_longas)}')
+else:
+    print('Nenhuma palavra longa encontrada.')
