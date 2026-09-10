a7c564acf293adbe319346e4ae5b7f52d776df3e
Victtorazzi <tiagovicttorazzi@gmail.com>
2026-09-10 19:03:40 -0300

    terminar pratica amanhã

--- pratica.py
+++ pratica.py
@@ -106,31 +106,119 @@
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-import random
+# import random
 
 
-def adivinha():
-    numero_secreto = random.randint(1,100)
-    tentativas = 0
+# def adivinha():
+#     numero_secreto = random.randint(1,100)
+#     tentativas = 0
 
-    while True: 
+#     while True: 
 
-        numero = int(input('Tente adivinhar o número (1-100): '))
+#         numero = int(input('Tente adivinhar o número (1-100): '))
 
-        if numero < 1 or numero > 100:
-            print('Entrada inválida: Número fora do intervalo! Digite um número entre 1 e 100.')
-            raise ValueError('Número fora do intervalo! Digite um número entre 1 e 100')
+#         if numero < 1 or numero > 100:
+#             print('Entrada inválida: Número fora do intervalo! Digite um número entre 1 e 100.')
+#             raise ValueError('Número fora do intervalo! Digite um número entre 1 e 100')
 
-        if numero < numero_secreto:
-            print('Muito Baixo! Tente novamente.')
-            tentativas += 1
+#         if numero < numero_secreto:
+#             print('Muito Baixo! Tente novamente.')
+#             tentativas += 1
 
-        elif numero > numero_secreto:
-            print('Muito Alto! Tente novamente.')
-            tentativas += 1
+#         elif numero > numero_secreto:
+#             print('Muito Alto! Tente novamente.')
+#             tentativas += 1
 
-        else:
-            print(f'Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativas.')
-            break
+#         else:
+#             print(f'Parabéns! Você acertou o número {numero_secreto} em {tentativas} tentativas.')
+#             break
 
-adivinha()
+# adivinha()
+
+#--------------------------------------------------------------------------------------------------------
+
+# def calculadora():
+#     primeiro = float(input('DIgite o primeiro número: '))
+#     operacao = input('Digite a operação (+, -, *, /): ')
+#     segundo = float(input('Digite o segundo número: '))
+
+#     try:
+#         if operacao == '+':
+#             resultado = primeiro + segundo
+#         elif operacao == '-':
+#             resultado = primeiro - segundo
+#         elif operacao == '*':
+#             resultado = primeiro * segundo
+#         elif operacao == '/':
+#             resultado = primeiro / segundo
+#         else:
+#             print('Opção inválida. Escolha uma operação.')
+#             return
+
+#         print(f'Resultado: {resultado}')
+
+#     except ValueError:
+#         print('Erro: Entrada inválida. Digite apenas números.')
+#     except ZeroDivisionError:
+#         print('Erro: Divisão por zero não é permitida.')
+# calculadora()
+
+#====================================================================
+
+# import os
+
+# def gerenciador_tarefas():
+#     tarefas = []
+
+#     while True:
+#         print('1. Adicionar tarefa')
+#         print('2. Visualizar tarefas')
+#         print('3. Remover tarefa')
+#         print('4. Sair')
+
+#         escolha = int(input('Escolha uma opção: '))
+
+#         if escolha == 1:
+#             os.system('cls')
+#             tarefa = input('Digite a tarefa: ').split()
+#             if tarefa:
+#                 tarefas.append(tarefa)
+#                 print('Tarefa adicionada!')
+#             else:
+#                 print('Erro: A tarefa não pode estar vazia.')
+            
+
+#         elif escolha == 2:
+#             os.system('cls')
+#             if tarefas:
+#                 print('Tarefas:')
+#                 for i, tarefa in enumerate(tarefas, 1):
+#                     print(f'{i}. {tarefa}')
+#             else:
+#                 print('Nenhuma tarefa cadastrada.')
+
+#         elif escolha == 3:
+#             os.system('cls')
+#             if tarefas:
+#                 try:
+#                     indice = int(input('Digite o número da tarefa a ser removida: '))
+#                     if 1 <= indice <= len(tarefas):
+#                         tarefa_removida = tarefas.pop(indice - 1)
+#                         print(f'Tarefa "{tarefa_removida}" removida!')
+#                     else:
+#                         print('Erro: Número inválido. Digite um número válido')
+#                 except ValueError:
+#                     print('Erro: Entrada inválida. Digite um número')
+
+#         elif escolha == 4:
+#             os.system('cls')
+#             print('Saindo do gerenciador de tarefas. Até mais!')
+#             break
+
+#         else:
+#             print('Erro: Opção inválida. Escolha uma opção entre 1 e 4')
+
+# gerenciador_tarefas()
+
+#===========================================================================
+
