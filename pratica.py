82cd68611f695bfdb2b6d75ae732d92def023109
Victtorazzi <tiagovicttorazzi@gmail.com>
2026-09-10 01:13:03 -0300

    terminar amanha

--- pratica.py
+++ pratica.py
@@ -40,15 +40,45 @@
 
 #=======================================================================
 
-texto = input('Digite um texto: ')
+# texto = input('Digite um texto: ')
 
-palavras_longas = []
+# palavras_longas = []
 
-for palavra in texto.split():
-    if len(palavra)>10:
-        palavras_longas.append(palavra)
+# for palavra in texto.split():
+#     if len(palavra)>10:
+#         palavras_longas.append(palavra)
 
-if palavras_longas:
-    print(f'Palavras longas encontradas: {", ".join(palavras_longas)}')
-else:
-    print('Nenhuma palavra longa encontrada.')
+# if palavras_longas:
+#     print(f'Palavras longas encontradas: {", ".join(palavras_longas)}')
+# else:
+#     print('Nenhuma palavra longa encontrada.')
+
+#--------------------------------------------------------------
+
+# import random
+
+# def gerar_senha():
+
+#     maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#     minusculas = 'abcdefghijklmnopqrstuvwxyz'
+#     numeros = '0123456789'
+#     especiais = '!@#$%&*'
+
+#     senha = [
+#         random.choice(maiusculas),
+#         random.choice(minusculas),
+#         random.choice(numeros),
+#         random.choice(especiais)
+#     ]
+
+#     todos_caracteres = maiusculas + minusculas + numeros + especiais
+#     senha.extend(random.choices(todos_caracteres, k=8))
+#     random.shuffle(senha)
+#     return ''.join(senha)
+
+# print(f'Senha gerada: {gerar_senha()}')
+
+import random
+
+def jokempo():
+    opcoes = 
